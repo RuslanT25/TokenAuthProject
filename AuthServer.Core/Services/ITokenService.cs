eee0689165217a841849c467a767ee6536b14120
Ruslan <tagizaderuslan25@gmail.com>
2024-07-21 12:01:32 +0400

    Added Token and Client DTO and added ITokenService

---
+++ AuthServer.Core/Services/ITokenService.cs
@@ -0,0 +1,18 @@
+﻿using AuthServer.Core.Configurations;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDTO CreateToken(AppUser user);
+
+        ClientTokenDTO CreateTokenByClient(Client client);
+    }
+}
