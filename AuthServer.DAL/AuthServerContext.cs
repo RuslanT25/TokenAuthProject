48f715ded33c617ecb15e4ceb18325b167f1e72d
Ruslan <tagizaderuslan25@gmail.com>
2024-07-21 16:30:52 +0400

    Added GenericRepository class and IUnitOfWork interface and UnitOfWork class

---
+++ AuthServer.DAL/AuthServerContext.cs
@@ -0,0 +1,22 @@
+﻿using AuthServer.Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.DAL
+{
+    public class AuthServerContext : IdentityDbContext<AppUser,IdentityRole,string>
+    {
+        public AuthServerContext(DbContextOptions<AuthServerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+    }
+}
---
+++ AuthServer.DAL/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using AuthServer.Core.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.DAL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AuthServerContext _context;
+
+        public UnitOfWork(AuthServerContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
