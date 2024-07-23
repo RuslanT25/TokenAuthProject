5eb1d94cacd7c3ffa89c6fcd72c646ac44ddef3d
Ruslan <tagizaderuslan25@gmail.com>
2024-07-23 10:14:18 +0400

    Added AutoMapper and BaseManager

---
+++ AuthServer.Business/Mappers/MapProfile.cs
@@ -0,0 +1,20 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Business.Mappers
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<ProductDTO, Product>().ReverseMap();
+            CreateMap<AppUserDTO, AppUser>().ReverseMap();
+        }
+    }
+}
