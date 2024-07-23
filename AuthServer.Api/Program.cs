3e80e9e559c0527b7d8b749839b52200cbc7207e
Ruslan <tagizaderuslan25@gmail.com>
2024-07-23 16:22:23 +0400

    Added Program DI configurations and token verification

--- AuthServer.Api/Program.cs
+++ AuthServer.Api/Program.cs
@@ -1,7 +1,16 @@
-
 using AuthServer.Business.Configurations;
 using AuthServer.Core.Configurations;
-using Microsoft.Extensions.Configuration;
+using AuthServer.Core.Repositories;
+using AuthServer.Core.Services;
+using AuthServer.Core.UnitOfWork;
+using AuthServer.DAL.Repositories;
+using AuthServer.DAL;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AuthServer.Business.Managers;
+using AuthServer.Core.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace AuthServer.Api
 {
@@ -18,9 +27,48 @@ namespace AuthServer.Api
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            // var tokenOptions = builder.Configuration.GetSection("TokenOption").Get<CustomTokenOption>();
+            builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+            builder.Services.AddScoped<IUserService, UserManager>();
+            builder.Services.AddScoped<ITokenService, TokenManager>();
+            builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            builder.Services.AddScoped(typeof(IBaseService<,>), typeof(BaseManager<,>));
+
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            builder.Services.AddDbContext<AuthServerContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+            });
+
+            builder.Services.AddIdentity<AppUser, IdentityRole>(Opt =>
+            {
+                Opt.User.RequireUniqueEmail = true;
+                Opt.Password.RequireNonAlphanumeric = false;
+            }).AddEntityFrameworkStores<AuthServerContext>().AddDefaultTokenProviders();
+
             builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
             builder.Services.Configure<List<Client>>(builder.Configuration.GetSection("Clients"));
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opts =>
+            {
+                var tokenOptions = builder.Configuration.GetSection("TokenOption").Get<CustomTokenOption>();
+                opts.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidIssuer = tokenOptions!.Issuer,
+                    ValidAudience = tokenOptions.Audience[0],
+                    IssuerSigningKey = SignManager.GetSymmetricSecurityKey(tokenOptions.SecurityKey),
+
+                    ValidateIssuerSigningKey = true,
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
 
             var app = builder.Build();
 
