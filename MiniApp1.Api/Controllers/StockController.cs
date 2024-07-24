15d48cb192e2cf4ac489dc1ca429a4ac86ecf6ba
Ruslan <tagizaderuslan25@gmail.com>
2024-07-24 18:19:36 +0400

    Added controllers for MiniApp1 and MiniApp2

---
+++ MiniApp1.Api/Controllers/StockController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MiniApp1.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStock()
+        {
+            var userName = User.Identity!.Name;
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+            return Ok($"Your username - {userName}, your userId - {userId}");
+        }
+    }
+}
---
+++ MiniApp2.Api/Controllers/InvoiceController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MiniApp2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInvoices()
+        {
+            var userName = User.Identity!.Name;
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+            return Ok($"Your username - {userName}, your userId - {userId}");
+        }
+    }
+}
