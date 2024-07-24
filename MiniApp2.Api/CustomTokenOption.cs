e8bef3c82143b15e2b5f9d0aacd0247560c8e4d1
Ruslan <tagizaderuslan25@gmail.com>
2024-07-24 18:03:27 +0400

    Added token configurations for MiniApi1, MiniApi2 and MiniApi3

---
+++ MiniApp2.Api/CustomTokenOption.cs
@@ -0,0 +1,13 @@
+﻿namespace MiniApp2.Api
+{
+    public class CustomTokenOption
+    {
+        public List<string> Audience { get; set; }
+        public string Issuer { get; set; }
+
+        public int AccessTokenExpiration { get; set; }
+        public int RefreshTokenExpiration { get; set; }
+
+        public string SecurityKey { get; set; }
+    }
+}
--- MiniApp2.Api/Program.cs
+++ MiniApp2.Api/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 namespace MiniApp2.Api
 {
     public class Program
@@ -14,6 +16,30 @@ namespace MiniApp2.Api
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
+            builder.Services.Configure<List<Client>>(builder.Configuration.GetSection("Clients"));
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opts =>
+            {
+                var tokenOptions = builder.Configuration.GetSection("TokenOption").Get<CustomTokenOption>();
+                opts.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidIssuer = tokenOptions!.Issuer,
+                    ValidAudience = tokenOptions.Audience[0],
+                    IssuerSigningKey = SignManager.GetSymmetricSecurityKey(tokenOptions.SecurityKey),
+
+                    ValidateIssuerSigningKey = true,
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -25,8 +51,8 @@ namespace MiniApp2.Api
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
